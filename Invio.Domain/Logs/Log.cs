2984500d8ab18413ad2f4e6f66fbca09ae125505
aldruin <abl.souza97@gmail.com>
2024-11-22 22:00:27 -0300

    fix

---
+++ Invio.Domain/Logs/Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Invio.Domain.Entities;
+
+namespace Invio.Domain.Logs;
+public class Log
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required Guid UsuarioId { get; set; }
+    public required string Acao { get; set; }
+    public required string EntidadeAfetada { get; set; }
+    public DateTime Data { get; set; } = DateTime.UtcNow;
+
+    public Log() { }
+
+
+    public Log(Guid usuarioId, string acao, string entidadeAfetada)
+    {
+        UsuarioId = usuarioId;
+        Acao = acao;
+        EntidadeAfetada = entidadeAfetada;
+    }
+
+    public override string ToString()
+    {
+        return $"{Data:yyyy-MM-dd HH:mm:ss} - Usuário ID: {UsuarioId} - Ação: {Acao} - Entidade: {EntidadeAfetada}";
+    }
+}
